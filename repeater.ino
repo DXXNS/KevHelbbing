51fc6a68d3cce7416c6040b064024cdb71731811
Reifensepp <108888172+DXXNS@users.noreply.github.com>
2026-02-09 11:24:57 +0100

    Update repeater.ino

--- repeater.ino
+++ repeater.ino
@@ -3,33 +3,38 @@
 
 uint8_t receiverMac[] = {0xD4, 0x8A, 0xFC, 0xA3, 0x59, 0xB8}; // main receiver MAC
 
-
 typedef struct msg {
   int value;
 } msg;
 
 msg data;
 
-void onReceive(const esp_now_recv_info *info, const uint8_t *incomingData, int len) {
-  memcpy(&data, incomingData, sizeof(data));
+void onReceive(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
+  if(len == sizeof(msg)) {
+    memcpy(&data, incomingData, sizeof(data));
+    Serial.print("Relay: ");
+    Serial.println(data.value);
 
-  Serial.print("relay: ");
-  Serial.println(data.value);
-
-  esp_now_send(receiverMac, (uint8_t*)&data, sizeof(data));
+    esp_now_send(receiverMac, (uint8_t*)&data, sizeof(data));
+  }
 }
 
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
 
-  esp_now_init();
+  if (esp_now_init() != 0) {
+    Serial.println("Fehler beim Initialisieren von ESP-NOW");
+    return;
+  }
 
   esp_now_peer_info_t peer = {};
   memcpy(peer.peer_addr, receiverMac, 6);
   peer.channel = 0;
   peer.encrypt = false;
-  esp_now_add_peer(&peer);
+  if(esp_now_add_peer(&peer) != ESP_OK) {
+    Serial.println("Fehler beim Hinzufügen des Peers");
+  }
 
   esp_now_register_recv_cb(onReceive);
 }
