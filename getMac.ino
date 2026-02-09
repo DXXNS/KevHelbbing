3d4b51d2e53ce72dbd1458fcbbaaa87b49a2fecd
Reifensepp <108888172+DXXNS@users.noreply.github.com>
2026-02-09 11:05:34 +0100

    Enhance MAC address logging in setup

--- getMac.ino
+++ getMac.ino
@@ -1,7 +1,14 @@
 #include <WiFi.h>
+
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+
   WiFi.mode(WIFI_STA);
-  Serial.println(WiFi.macAddress());
+
+  String mac = WiFi.macAddress();
+  Serial.print("mac: ");
+  Serial.println(mac);
 }
+
 void loop() {}
